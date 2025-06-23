c32fa2adde98aa68fd06953327eb18db67c6164f
fxpby <fxpby@outlook.com>
2025-06-23 23:54:58 +0800

    feat: leftBar

--- client/src/components/leftBar/leftBar.jsx
+++ client/src/components/leftBar/leftBar.jsx
@@ -1,7 +1,30 @@
 import s from "./leftBar.module.less";
 
 const LeftBar = () => {
-  return <div className={s.leftBar}>LeftBar</div>;
+  return (
+    <div className={s.leftBar}>
+      <div className={s.menuIcons}>
+        <a href="/" className={s.menuIcon}>
+          <img src="/general/logo.png" alt="" />
+        </a>
+        <a href="/" className={s.menuIcon}>
+          <img src="/general/home.svg" alt="" />
+        </a>
+        <a href="/" className={s.menuIcon}>
+          <img src="/general/create.svg" alt="" />
+        </a>
+        <a href="/" className={s.menuIcon}>
+          <img src="/general/updates.svg" alt="" />
+        </a>
+        <a href="/" className={s.menuIcon}>
+          <img src="/general/messages.svg" alt="" />
+        </a>
+      </div>
+      <a href="/" className={s.menuIcon}>
+        <img src="/general/messages.svg" alt="" />
+      </a>
+    </div>
+  );
 };
 
 export default LeftBar;
