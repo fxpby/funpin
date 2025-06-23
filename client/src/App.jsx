12bb75f6c93eccf17d4d3a2199716daa7a6613ad
fxpby <fxpby@outlook.com>
2025-06-23 23:45:02 +0800

    feat: main page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,18 @@
-import './app.css'
+import s from "./app.module.less";
+import Gallery from "./components/gallery/gallery";
+import LeftBar from "./components/leftBar/leftBar";
+import TopBar from "./components/topBar/topBar";
 
 const App = () => {
   return (
-    <div>Hello World</div>
-  )
-}
+    <div className={s.app}>
+      <LeftBar />
+      <div className={s.content}>
+        <TopBar />
+        <Gallery />
+      </div>
+    </div>
+  );
+};
 
-export default App
+export default App;
